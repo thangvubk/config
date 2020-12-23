ba7377bb6c8fa9cb1facd494b4ba19ef02cfa6db
Thang Vu <thangvubk@gmail.com>
2020-12-23 18:34:00 +0900

    add install env

---
+++ install_env.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+apt update
+apt install -y vim htop git tmux cmake build-essential wget python3 python3-dev
+
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+git clone https://github.com/preservim/nerdtree.git ~/.vim/bundle/nerdtree
+git clone https://github.com/Vimjas/vim-python-pep8-indent.git ~/.vim/bundle/vim-python-pep8-indent
+git clone https://github.com/crusoexia/vim-monokai.git ~/.vim/bundle/vim-monokai
+git clone https://github.com/ycm-core/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
+git clone https://github.com/airblade/vim-gitgutter.git ~/.vim/bundle/vim-gitgutter
+git clone https://github.com/nvie/vim-flake8.git ~/.vim/bundle/vim-flake8
+
+
+cd ~
+wget https://raw.githubusercontent.com/thangvubk/config/master/.vimrc
+wget https://raw.githubusercontent.com/thangvubk/config/master/.tmux.conf
+wget https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh
